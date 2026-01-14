f6a67556fe88a4aa227320e7f5e234a11e6f4399
AnasNEJMI <anasnejmi@gmail.com>
2026-01-14 15:19:39 +0100

    test : wired vitest, created unit test for hash, created script commands for running tests

---
+++ backend/tests/unit/hash.test.ts
@@ -0,0 +1,18 @@
+import { hashPassword, verifyPassword } from "../../src/utils/auth";
+
+import {describe, it, expect} from 'vitest';
+
+describe('Password Hashing', () => {
+    it("hashes and verifies a password correctly", async () => {
+        const password = "StrongPassword123!";
+        
+        const hash = await hashPassword(password);
+        expect(hash).not.toBe(password);
+        
+        const isValid = await verifyPassword(password, hash);
+        expect(isValid).toBe(true);
+        
+        const isInvalid = await verifyPassword("wrongpassword", hash);
+        expect(isInvalid).toBe(false);
+    })
+})
