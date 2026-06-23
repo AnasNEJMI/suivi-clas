92d0e2d9a9edd0f4fc777ffafe35b29b7beb80de
AnasNEJMI <anasnejmi@gmail.com>
2026-06-23 15:32:22 +0200

    pre-deploy fixes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,7 @@
 import app from "./app.js";
 
-const SERVER_PORT = process.env.SERVER_PORT;
-if (!SERVER_PORT) {
-  throw new Error("SERVER_PORT is not defined")
-}
+const PORT = process.env.PORT || process.env.SERVER_PORT || 3000;
 
-app.listen(SERVER_PORT, () => {
-    console.log(`Node server running on https://localhost:${SERVER_PORT}`);
+app.listen(PORT, () => {
+    console.log(`Node server running on https://localhost:${PORT}`);
 })
